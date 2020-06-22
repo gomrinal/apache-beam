0164f14c55275985f5b56b47de8ae86f02ff11d4
Mrinal <mrinaal1234@gmail.com>
2020-06-21 22:45:34 -0700

    updated code

--- project-1/attendance_calculator.py
+++ project-1/attendance_calculator.py
@@ -6,27 +6,21 @@ def SplitRow(element):
 def filtering(record):
   return record[3] == 'Accounts'
 
+INPUT_DIR='data/attendance_data.txt'
+OUTPUT_DIR='output/output'
 
 p1 = beam.Pipeline()
 
 attendance_count = (
     
    p1
-    |beam.io.ReadFromText('data.txt')
+    |beam.io.ReadFromText(INPUT_DIR)
     |beam.Map(SplitRow)
-   #| beam.Map(lambda record: record.split(','))
-
     |beam.Filter(filtering)
-  # |beam.Filter(lambda record: record[3] == 'Accounts')
-    
     |beam.Map(lambda record: (record[1], 1))
     |beam.CombinePerKey(sum)
-    
-    |beam.io.WriteToText('data/output_new_final')
+    |beam.io.WriteToText(OUTPUT_DIR)
   
 )
 
 p1.run()
-
-# Sample the first 20 results, remember there are no ordering guarantees.
-# {('head -n 20 data/output_new_final-00000-of-00001')}
--- project-2/wordcount.py
+++ project-2/wordcount.py
@@ -2,12 +2,16 @@ import apache_beam as beam
 import re
 p1 = beam.Pipeline()
 
+
+INPUT_DIR='data/wordcount_data.txt'
+OUTPUT_DIR='output/output'
+
 wordcount = (
     p1
-    | beam.io.ReadFromText('wordcount_data.txt')
+    | beam.io.ReadFromText(INPUT_DIR)
     | 'Find words' >> beam.FlatMap(lambda line: re.findall(r"[a-zA-Z']+", line))
     | 'Pair words with 1' >> beam.Map(lambda word: (word, 1))
     | 'Group and sum' >> beam.CombinePerKey(sum)
-    | beam.io.WriteToText('output/output')
+    | beam.io.WriteToText(OUTPUT_DIR)
 )
 p1.run()
