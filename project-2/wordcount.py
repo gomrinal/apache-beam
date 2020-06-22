e3b647f07e70a1e18f39c0e9d8e9bfcfe30e9695
Mrinal <mrinaal1234@gmail.com>
2020-06-21 22:39:46 -0700

    wordcount

--- project-2/wordcount.py
+++ project-2/wordcount.py
@@ -1,13 +1,13 @@
 import apache_beam as beam
-
+import re
 p1 = beam.Pipeline()
 
 wordcount = (
     p1
     | beam.io.ReadFromText('wordcount_data.txt')
-    | beam.Map(lambda record: record.split())
-    | beam.Map(lambda record: len(record))
-    | beam.CombineGlobally(sum)
+    | 'Find words' >> beam.FlatMap(lambda line: re.findall(r"[a-zA-Z']+", line))
+    | 'Pair words with 1' >> beam.Map(lambda word: (word, 1))
+    | 'Group and sum' >> beam.CombinePerKey(sum)
     | beam.io.WriteToText('output/output')
 )
 p1.run()
